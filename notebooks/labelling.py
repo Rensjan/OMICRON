af423b266d0a296c1970f73e54d3551418efbedf
seba <sebastien85@hotmail.be>
2023-12-06 01:39:03 +0100

    made a histogram for R,G,B

---
+++ notebooks/labelling.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import glob
+
+sys.path.insert(1, os.path.join("..", "data")) # access to data folder
+sys.path.insert(1, os.path.join("..", "utils"))# access to utils folder
+
+from label_utils import image_analysis
+
+# image_path = glob.glob(os.path.join(image_name))
+image_path = "../data/png_unfiltered/51689389222_7e91bc6cf8_o'_half_2.png"
+
+
+image = image_analysis(image_path)
+equalized_tensor = image.plotTensorEqualized()
+tensor = image.plotTensor()
+
+image.RGB_distribution(tensor)
+
+# image.plotImage_url()
+
+
+
