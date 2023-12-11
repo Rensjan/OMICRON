ea7a7bb23ab4767f58afd9b615a9aab3bf08df31
seba <sebastien85@hotmail.be>
2023-12-11 09:10:18 +0100

    updated labelling.py & label_utils.py

--- notebooks/labelling.py
+++ notebooks/labelling.py
@@ -12,10 +12,19 @@ image_path = "../data/png_unfiltered/51689389222_7e91bc6cf8_o'_half_2.png"
 
 
 image = image_analysis(image_path)
-equalized_tensor = image.plotTensorEqualized()
-tensor = image.plotTensor()
+# equalized_tensor = image.plotTensorEqualized()
 
-image.RGB_distribution(tensor)
+svd_tensor = image.svd()
+image.plotTensor(svd_tensor)
+
+
+
+
+# tensor = image.plotTensor()
+# image.svd(tensor)
+
+
+#image.RGB_distribution(tensor)
 
 # image.plotImage_url()
 
